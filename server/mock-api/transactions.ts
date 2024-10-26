68a43db9acd3519434a17549b2735adf7c77ba88
MrCelestis <slamol92@yandex.ru>
2024-10-26 23:53:11 +0400

    Mock api logging

--- server/mock-api/transactions.ts
+++ server/mock-api/transactions.ts
@@ -1,5 +1,6 @@
 import db from '../../mock-server/db.json';
 
 export default defineEventHandler((event) => {
+  console.log('MOCK API: transactions', event.toJSON());
   return db.transactions;
 });
