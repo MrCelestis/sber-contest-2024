5e346345cf0d898ecd03557f60829be3b79d6320
MrCelestis <slamol92@yandex.ru>
2024-10-27 23:14:16 +0400

    A couple more tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { configDefaults } from 'vitest/config';
 export default defineVitestConfig({
   test: {
     //e2e tests run using playwright directly, it's much more convenient
-    exclude: [...configDefaults.exclude, 'test/e2e/*']
+    exclude: [...configDefaults.exclude, 'test/e2e/*'],
+    coverage: {
+      provider: 'istanbul'
+    }
   }
 });
